0396d0933d0839d41060d7e1a8d4e7381eb9235a
kormannj <33202837+kormannj@users.noreply.github.com>
2018-11-22 17:02:20 -0200

    Add files via upload

--- kaggle_titanic_final_00.py
+++ kaggle_titanic_final_00.py
@@ -2,25 +2,25 @@ import pandas as pd
 
 #Importar os dados de treino e de teste
 base_treinamento = pd.read_csv('train.csv')
-#base_teste = pd.read_csv('test.csv')
+base_teste = pd.read_csv('test.csv')
 
 #Excluir colunas desnecessárias
 #axis=1 - elimina a coluna inteira
 #inplace=True - atualiza o DataSet
 base_treinamento.drop(['Name','Ticket','Cabin'], axis=1, inplace=True)
-#base_teste.drop(['Name','Ticket','Cabin'], axis=1, inplace=True)
+base_teste.drop(['Name','Ticket','Cabin'], axis=1, inplace=True)
 
 #Transformando os atributos categóricos em dummies
 new_base_treinamento = pd.get_dummies(base_treinamento)
-#new_base_teste = pd.get_dummies(base_teste)
+new_base_teste = pd.get_dummies(base_teste)
 
 #Comando para verificar se há valores nulos em alguma coluna. Pode ficar comentado depois.
 #null_treino = new_base_treino.isnull().sum().sort_values(ascending=False)
 #null_teste = new_base_teste.isnull().sum().sort_values(ascending=False)
 
 new_base_treinamento['Age'].fillna(new_base_treinamento['Age'].mean(), inplace=True)
-#new_base_teste['Age'].fillna(new_base_teste['Age'].mean(), inplace=True)
-#new_base_teste['Fare'].fillna(new_base_teste['Fare'].mean(), inplace=True)
+new_base_teste['Age'].fillna(new_base_teste['Age'].mean(), inplace=True)
+new_base_teste['Fare'].fillna(new_base_teste['Fare'].mean(), inplace=True)
 
 #Dividir os atributos em previsores e classe (classe é o que eu quero prever)
 previsores = new_base_treinamento.drop('Survived', axis=1)
@@ -29,7 +29,7 @@ classe = new_base_treinamento['Survived']
 #Fazer a divisão entre dados de treino e teste
 from sklearn.model_selection import train_test_split
 
-previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+#previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
 
 #Naive Bayes: 79,85%
 #from sklearn.naive_bayes import GaussianNB
@@ -45,10 +45,10 @@ previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = tra
 #previsoes = classificador.predict(previsores_teste)
 
 #Random Forest: 86,57%
-#from sklearn.ensemble import RandomForestClassifier
-#classificador = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
-#classificador.fit(previsores_treinamento, classe_treinamento)
-#previsoes = classificador.predict(previsores_teste)
+from sklearn.ensemble import RandomForestClassifier
+classificador = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
+classificador.fit(previsores, classe)
+previsoes = classificador.predict(new_base_teste)
 
 #kNN: 64,92%
 #from sklearn.neighbors import KNeighborsClassifier
@@ -74,16 +74,14 @@ previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = tra
 #classificador.fit(previsores_treinamento, classe_treinamento)
 #previsoes = classificador.predict(previsores_teste)
 
-#GBM: 83,52%
-from sklearn.ensemble import GradientBoostingClassifier
-classificador = GradientBoostingClassifier(n_estimators=100, criterion='friedman_mse', random_state=0)
-    #(n_estimators=100, criterion='entropy', random_state=0)
-classificador.fit(previsores_treinamento, classe_treinamento)
-previsoes = classificador.predict(previsores_teste)
+#from sklearn.metrics import confusion_matrix, accuracy_score
+#precisao = accuracy_score(classe_teste, previsoes)
+#matriz = confusion_matrix(classe_teste, previsoes)
 
-from sklearn.metrics import confusion_matrix, accuracy_score
-precisao = accuracy_score(classe_teste, previsoes)
-matriz = confusion_matrix(classe_teste, previsoes)
+arquivo_final = pd.DataFrame()
+arquivo_final['PassengerId'] = new_base_teste['PassengerId']
+arquivo_final['Survived'] = previsoes
+arquivo_final.to_csv('submission.csv', index=False)
 
 print('Fim')
 
